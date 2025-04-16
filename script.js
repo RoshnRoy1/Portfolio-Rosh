7a49a92c1f3644d601db9e191346e1d62f96e3a4
RoshnRoy1 <84830615+RoshnRoy1@users.noreply.github.com>
2025-04-17 05:01:44 +0530

    final portfolio

---
+++ script.js
@@ -0,0 +1,13 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+
+  const typed = new Typed('#typevfx', {
+    strings: ['Frontend Developer', 'Python Developer','FullStack Developer','WebApp Developer','UI/UX Developer','Tech Enthusiast' ],
+    typeSpeed: 60,
+    backSpeed: 60,
+    backDelay: 100,
+  });
